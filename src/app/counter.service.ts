626369e80169c25e2323af0bbab98492fdc89bc6
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-12-21 23:07:22 +0530

    add a method incrementActiveToInactive()

--- src/app/counter.service.ts
+++ src/app/counter.service.ts
@@ -1,4 +1,9 @@
 export class CounterService {
   activeToInactiveCounter = 0;
   InactiveToActiveCounter = 0;
+
+  incrementActiveToInactive () {
+    this.activeToInactiveCounter++;
+    console.log(this.activeToInactiveCounter);
+  }
 }
