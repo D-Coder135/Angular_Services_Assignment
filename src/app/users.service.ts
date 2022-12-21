a447f2aa03d25009c0bd7c4860255c1e3347abb2
Devansh K Shukla <devansh.shukla_cs19@gla.ac.in>
2022-12-21 23:11:08 +0530

    add the constructor()

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,10 +1,13 @@
 import {Injectable} from "@angular/core";
+import {CounterService} from "./counter.service";
 
 @Injectable()
 export class UserService {
   activeUsers = ['Max', 'Anna'];
   inactiveUsers = ['Chris', 'Manu'];
 
+  constructor(private counterService: CounterService) {
+  }
   setToActive(id: number) {
     this.activeUsers.push(this.inactiveUsers[id]);
     this.inactiveUsers.splice(id, 1);
